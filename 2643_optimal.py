aaa1d3772bd70f62f61489143f44d7f4af0c0a34
GenryEden <a926788@gmail.com>
2021-04-05 19:26:51 +0300

    added 2643 optimal

---
+++ 2643_optimal.py
@@ -0,0 +1,15 @@
+memory = {}
+n = int(input())
+
+for _ in range(n):
+	inp = int(input())
+	if inp in memory:
+		memory[inp] += 1
+	else:
+		memory[inp] = 1
+ans = 0
+for x in memory:
+	if 100 - x in memory:
+		ans += min(memory[x], memory[100-x])
+
+ans //= 2
